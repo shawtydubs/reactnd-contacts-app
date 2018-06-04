f639f7ea331f35082c6df11eab4f06d5bf798cd8
Claire Walters <clairedwalters@gmail.com>
2018-06-04 17:12:10 -0400

    move contacts into state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import ListContacts from './ListContacts'
+import _ from 'lodash';
 
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        id: "karen",
+        name: "Karen Isgrigg",
+        handle: "karen_isgrigg",
+        avatarURL: "http://localhost:5001/karen.jpg"
+      },
+      {
+        id: "richard",
+        name: "Richard Kalehoff",
+        handle: "richardkalehoff",
+        avatarURL: "http://localhost:5001/richard.jpg"
+      },
+      {
+        id: "tyler",
+        name: "Tyler McGinnis",
+        handle: "tylermcginnis",
+        avatarURL: "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  };
+
+  removeContact = (contact) => {
+    this.setState(currentState => ({
+      contacts: _.reject(currentState.contacts, stateContact => (stateContact.id === contact.id))
+    }))
+  };
+
   render() {
     return (
-      <div>
-        Hello World
-      </div>
+      <ListContacts
+        contacts={this.state.contacts}
+        onContactDelete={this.removeContact}
+      />
     );
   }
 }
