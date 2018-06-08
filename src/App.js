5bdeb85c4646442932cbc2e442b0b4032a8ec891
Claire Walters <clairedwalters@gmail.com>
2018-06-08 11:21:02 -0400

    add API and some routing

--- src/App.js
+++ src/App.js
@@ -1,43 +1,38 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
-import _ from 'lodash';
+import CreateContact from './CreateContact'
+import _ from 'lodash'
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        id: "karen",
-        name: "Karen Isgrigg",
-        handle: "karen_isgrigg",
-        avatarURL: "http://localhost:5001/karen.jpg"
-      },
-      {
-        id: "richard",
-        name: "Richard Kalehoff",
-        handle: "richardkalehoff",
-        avatarURL: "http://localhost:5001/richard.jpg"
-      },
-      {
-        id: "tyler",
-        name: "Tyler McGinnis",
-        handle: "tylermcginnis",
-        avatarURL: "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    contacts: [],
+    screen: 'create'
   };
+
+  componentDidMount() {
+    ContactsAPI.getAll().then(contacts => {
+      this.setState({contacts})
+    })
+  }
 
   removeContact = (contact) => {
     this.setState(currentState => ({
       contacts: _.reject(currentState.contacts, stateContact => (stateContact.id === contact.id))
     }));
+
+    ContactsAPI.remove(contact)
   };
 
   render() {
     return (
-      <ListContacts
-        contacts={this.state.contacts}
-        onContactDelete={this.removeContact}
-      />
+      <div>
+        {this.state.screen === 'list' && <ListContacts
+          contacts={this.state.contacts}
+          onContactDelete={this.removeContact}
+        />}
+        {this.state.screen === 'create' && <CreateContact />}
+      </div>
     );
   }
 }
